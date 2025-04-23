8782fd8f45f01eb446b3334ce751bd42d133911f
Larry Gray <lwgray@gmail.com>
2025-04-23 11:36:21 -0600

    setup Heroku flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -39,6 +39,9 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+    # Get the PORT from environment variable for Heroku
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- flask_app/config.py
+++ flask_app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-for-development')
-    STRAPI_URL = os.environ.get('STRAPI_URL', 'http://192.168.0.29:1337')
+    STRAPI_URL = os.environ.get('STRAPI_URL', 'http://0.0.0.0:1337')
     STRAPI_API_TOKEN = os.environ.get('STRAPI_API_TOKEN', '')
