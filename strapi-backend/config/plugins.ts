0f4fe61e387c41034be8da6d5fdf1718a8c946b8
Larry Gray <lwgray@gmail.com>
2025-04-23 17:55:32 -0600

    Fix upload to s3 problem... check out image url display issue

--- strapi-backend/config/plugins.ts
+++ strapi-backend/config/plugins.ts
@@ -1,21 +1,31 @@
-export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          ACL: env('AWS_ACL', 'public-read'),
-          Bucket: env('AWS_BUCKET'),
+export default ({ env }) => {
+  // Debug logging
+  console.log('AWS_ACCESS_KEY_ID exists:', !!env('AWS_ACCESS_KEY_ID'));
+  console.log('AWS_SECRET_ACCESS_KEY exists:', !!env('AWS_SECRET_ACCESS_KEY'));
+  console.log('AWS_REGION exists:', !!env('AWS_REGION'));
+  console.log('AWS_BUCKET exists:', !!env('AWS_BUCKET'));
+
+  // Return the configuration object
+  return {
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID') || env('AWS_SECRET_ACCESS_KEY'),
+            secretAccessKey: env('AWS_SECRET_ACCESS_KEY') || env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+            params: {
+              Bucket: env('AWS_BUCKET'),
+            }
+          }
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
         },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
     },
-  },
-});
+  };
+};
