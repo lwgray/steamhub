0ce9deb0bbc05ad3cc900cc8759a55ea06eaa9b2
Larry Gray <lwgray@gmail.com>
2025-04-23 15:05:57 -0600

    Setup Domain and s3

--- flask_app/config.py
+++ flask_app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-for-development')
-    STRAPI_URL = os.environ.get('STRAPI_URL', 'https://streamhub-strapi-4cf7326316ac.herokuapp.com')
+    STRAPI_URL = os.environ.get('STRAPI_URL', 'https://cms.maxthomasgray.com')
     STRAPI_API_TOKEN = os.environ.get('STRAPI_API_TOKEN', '')
--- strapi-backend/config/server.ts
+++ strapi-backend/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://streamhub-strapi-4cf7326316ac.herokuapp.com'),
+  url: env('PUBLIC_URL', 'https://cms.maxthomasgray.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
